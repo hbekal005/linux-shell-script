c717dff1400b73c30da5cc3bb9836646ee8cf726
hbekal005 <hbekal005@gmail.com>
2025-01-19 11:51:16 -0500

    updated the script

--- create-shared-dir.sh
+++ create-shared-dir.sh
@@ -4,7 +4,7 @@ echo "Script execution Started."
 
 # Check if at least one argument is provided
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <absolute or relative path>"
+    echo "Usage: $0 <absolute path>"
     exit 1
 fi
 
